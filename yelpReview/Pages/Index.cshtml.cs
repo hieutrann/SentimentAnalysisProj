93b704c986b809d819a82d7c91c6e81154220488
hieutrann <hieu.nghiem@okstate.edu>
2020-04-04 00:45:14 -0500

    Add project files.

---
+++ yelpReview/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.ML;
+using YelpReviewML.Model;
+
+namespace yelpReview.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public IndexModel(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetAnalyzeSentiment([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("Neutral");
+            var input = new ModelInput { Sentiment = text };
+            var prediction = _predictionEnginePool.Predict(input);
+            var sentiment = Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative";
+            return Content(sentiment);
+
+        }
+
+    }
+}
